fda986a3b4f3bde7727ddaf08d21d6f5e6b9de29
imnewt <maitantruc1999@gmail.com>
2019-09-22 21:16:09 +0700

    template layout

---
+++ 10.template-layout/controllers/user.controller.js
@@ -0,0 +1,39 @@
+const shortid = require('shortid');
+
+var db = require('../db');
+
+module.exports.index = function(req, res) {
+	res.render('users/index', {
+		users: db.get("users").value()
+	});
+};
+
+module.exports.search = function(req, res) {
+	let q = req.query.q;
+	let matchedUsers = db.get("users").value().filter(function(user) {
+		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+	});
+	res.render('users/index', {
+		users: matchedUsers
+	});
+};
+
+module.exports.create = function(req, res) {
+	res.render('users/create');
+};
+
+module.exports.get = function(req, res) {
+	var id = req.params.id;
+
+	var user = db.get('users').find({ id: id }).value();
+
+	res.render('users/view', {
+		user: user
+	});
+};
+
+module.exports.postCreate = function(req, res) {
+	req.body.id = shortid.generate();
+	db.get("users").push(req.body).write();
+	res.redirect('/users');
+};
