205f67ff74c290aaccb27fc74417367b0d39c24d
imnewt <maitantruc1999@gmail.com>
2019-09-26 21:34:36 +0700

    middleware

---
+++ 13.middleware/routes/user.route.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+var controller = require('../controllers/user.controller');
+var validate = require('../validate/user.validate');
+
+var router = express.Router();
+
+router.get('/', controller.index);
+
+router.get('/search', controller.search);
+
+router.get("/create", controller.create);
+
+router.get("/:id", controller.get);
+
+router.post("/create", validate.postCreate, controller.postCreate);
+
+module.exports = router;
