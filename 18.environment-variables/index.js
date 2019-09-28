c6f62776f98672b0523bc0a84133f727ef9f729a
imnewt <maitantruc1999@gmail.com>
2019-09-28 21:05:50 +0700

    enviroment variables

---
+++ 18.environment-variables/index.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+
+console.log(process.env.SESSION_SECRET);
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const userRoute = require('./routes/user.route');
+const authRoute = require('./routes/auth.route');
+
+const authMiddleware = require('./middlewares/auth.middleware');
+
+const port = 3000;
+
+const app = express()
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cookieParser('secret'));
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+	res.render('index', {
+		name: 'Newt'
+	});
+});
+
+app.use('/users', authMiddleware.requireAuth, userRoute);
+app.use('/auth', authRoute);
+
+
+app.listen(port, () => console.log('Example app listening on port ' + port))
